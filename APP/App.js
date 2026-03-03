b68001771281c964da4114cc333a1ef01c0ad70b
Fael <shaolindopneu68@gmail.com>
2026-03-03 15:13:39 +0000

    base da aula02

--- APP/App.js
+++ APP/App.js
@@ -2,30 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+	var requestOptions = {
+		method: 'GET',
+		redirect: 'follow'
+	};
 
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+	fetch("https://supreme-meme-r46qj5xg45qp3x9v4-3000.app.github.dev/posts", requestOptions)
+	.then(response => response.json())
+	.then(result => console.log(result))
+	.catch(error => console.log('error', error));
 
-  fetch("http://localhost:3000/posts", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-  return (
-    <View style={styles.container}>
-      <Text>fael fede</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>fael fede</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center', 
+		justifyContent: 'center',
+	},
 });
